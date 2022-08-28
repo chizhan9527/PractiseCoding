7fd6bbfdd8236e5e1c3d4dd139777f50ac706f75
chizhan9527 <1346885811@qq.com>
2022-08-28 22:21:51 +0800

    Update 27.移除元素.cpp

--- coding/27.移除元素.cpp
+++ coding/27.移除元素.cpp
@@ -14,13 +14,13 @@ public:
         for (int i = 0; i < size; i++)
         {
             if (nums[i] == val)
-            { // 发现需要移除的元素，就将数组集体向前移动一位
+            {
                 for (int j = i + 1; j < size; j++)
                 {
                     nums[j - 1] = nums[j];
                 }
-                i--;    // 因为下标i以后的数值都向前移动了一位，所以i也向前移动一位
-                size--; // 此时数组的大小-1
+                i--;
+                size--;
             }
         }
         return size;
