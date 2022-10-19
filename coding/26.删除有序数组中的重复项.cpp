c5ead49b3cfef0dd16836be0138873ec321342fb
chizhan9527 <1346885811@qq.com>
2022-10-19 20:21:19 +0800

    26

--- coding/26.删除有序数组中的重复项.cpp
+++ coding/26.删除有序数组中的重复项.cpp
@@ -5,11 +5,24 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    int removeDuplicates(vector<int>& nums) {
-
+    int removeDuplicates(vector<int> &nums)
+    {
+        if (nums.size() == 0)
+            return 0;
+        int p = 0;
+        int q = 1;
+        for (; q <= nums.size() - 1; q++)
+        {
+            if (nums[p] != nums[q])
+            {
+                nums[p + 1] = nums[q];
+                p++;
+            }
+        }
+        return p + 1;
     }
 };
 // @lc code=end
-
